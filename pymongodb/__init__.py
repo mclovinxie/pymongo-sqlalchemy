08d677e2908383b882b84b5577f9978f45d595a4
xihao.xie <xxh@VW101954>
2021-05-08 15:44:29 +0800

    [INIT] Project Initialization

--- pymongodb/__init__.py
+++ pymongodb/__init__.py
@@ -6,6 +6,7 @@ from .errors import *
 paramstyle = 'pyformat'
 threadsafety = 2
 
+
 __all__ = [
     'connect', 'apilevel', 'threadsafety', 'paramstyle',
     'Warning', 'Error', 'InterfaceError', 'DatabaseError', 'DataError', 'OperationalError', 'IntegrityError',
